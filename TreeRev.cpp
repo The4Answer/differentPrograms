e8af71040049b23bce385eeedb09d5cddd1b001a
The4Answer <67497475+The4Answer@users.noreply.github.com>
2023-03-31 13:07:27 +0300

    Create TreeRev.cpp
    Just small program))
    If I ever need this

---
+++ TreeRev.cpp
@@ -0,0 +1,54 @@
+//#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <string>
+#include <deque>
+#include <stack>
+#include <set>
+#include <map>
+#include <deque>
+#include <queue>
+#include <ctime>
+
+
+
+using namespace std;
+
+ifstream cin("input.txt");
+ofstream cout("output.txt");
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+ 
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (root == nullptr)
+            return root;
+        TreeNode* left = invertTree(root->left);
+        TreeNode* right = invertTree(root->right);
+        root->left = right;
+        root->right = left;
+        return root;
+    }
+};
+
+int main() {
+    /*
+    1) Read input data
+    2) Make Tree
+    3) Call Tree revert
+    4) Print result
+    */
+
+	return 0;
+}
